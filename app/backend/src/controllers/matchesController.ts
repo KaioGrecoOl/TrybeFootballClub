a9685d37ed82e1ea30ae51bc204b36ad213caeb3
Kaio G <kaiogreco91@gmail.com>
2022-09-01 02:07:14 -0300

    fix(Try catch): adiciona nos arquivos de matchescontroller e teamscontroller try catch

--- app/backend/src/controllers/matchesController.ts
+++ app/backend/src/controllers/matchesController.ts
@@ -1,35 +1,55 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import matchesService from '../services/matchesService';
 
 export default class MatchesController {
-  static getAllMatchesController = async (req: Request, res: Response) => {
-    const matche = await matchesService.getAllMAtchesService();
-    return res.status(200).json(matche);
-  };
-
-  static createMatchesController = async (req: Request, res: Response) => {
-    const createMatch = await matchesService.createMatchesService(
-      req.body,
-      req.body.awayTeam,
-      req.body.homeTeam,
-    );
-    if (req.body.awayTeam === req.body.homeTeam) {
-      return res.status(401).json({
-        message: 'It is not possible to create a match with two equal teams' });
+  static getAllMatchesController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const matche = await matchesService.getAllMAtchesService();
+      return res.status(200).json(matche);
+    } catch (err) {
+      next(err);
     }
-    if (createMatch === 'There is no such id') {
-      return res.status(404).json({ message: 'There is no team with such id!' });
+  };
+
+  static createMatchesController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const createMatch = await matchesService.createMatchesService(
+        req.body,
+        req.body.awayTeam,
+        req.body.homeTeam,
+      );
+      if (req.body.awayTeam === req.body.homeTeam) {
+        return res.status(401).json({
+          message: 'It is not possible to create a match with two equal teams' });
+      }
+      if (createMatch === 'There is no such id') {
+        return res.status(404).json({ message: 'There is no team with such id!' });
+      }
+      return res.status(201).json(createMatch);
+    } catch (err) {
+      next(err);
     }
-    return res.status(201).json(createMatch);
   };
 
-  static updateMatchesProgreesController = async (req: Request, res: Response) => {
-    await matchesService.updateMatchesProgressService(+req.params.id);
-    res.status(200).json({ message: 'Finished' });
+  static updateMatchesProgreesController = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      await matchesService.updateMatchesProgressService(+req.params.id);
+      res.status(200).json({ message: 'Finished' });
+    } catch (err) {
+      next(err);
+    }
   };
 
-  static updateMatchesInProgrees = async (req: Request, res: Response) => {
-    await matchesService.updateMatchesInProgress(req.body, +req.params.id);
-    res.status(200).json({ message: 'Updated' });
+  static updateMatchesInProgrees = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await matchesService.updateMatchesInProgress(req.body, +req.params.id);
+      res.status(200).json({ message: 'Updated' });
+    } catch (err) {
+      next(err);
+    }
   };
 }
--- app/backend/src/controllers/teamsController.ts
+++ app/backend/src/controllers/teamsController.ts
@@ -1,15 +1,23 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import teamsService from '../services/teamsService';
 
 export default class TeamsController {
-  static getAllTeamsController = async (req: Request, res: Response) => {
-    const team = await teamsService.getAllTeamsService();
-    return res.status(200).json(team);
+  static getAllTeamsController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const team = await teamsService.getAllTeamsService();
+      return res.status(200).json(team);
+    } catch (err) {
+      next(err);
+    }
   };
 
-  static getByIdController = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const team = await teamsService.getByIdService(Number(id));
-    return res.status(200).json(team);
+  static getByIdController = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const team = await teamsService.getByIdService(Number(id));
+      return res.status(200).json(team);
+    } catch (err) {
+      next(err);
+    }
   };
 }
